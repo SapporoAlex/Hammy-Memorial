508275adef3b7ed146e02d0aa9a2da82e57a730d
Alex_McKinley <alexanderfromaustralia@gmail.com>
2025-09-13 18:56:10 +0900

    Update gallery thumbnails and modal layout

--- rename.py
+++ rename.py
@@ -13,7 +13,7 @@ def rename_files(folder_path):
         # Get file extension
         _, ext = os.path.splitext(filename)
         # Create new name
-        new_name = f"hammy{i}{ext.lower()}"
+        new_name = f"hammy{i + 87}{ext.lower()}"
         # Build full paths
         old_path = os.path.join(folder_path, filename)
         new_path = os.path.join(folder_path, new_name)
@@ -22,5 +22,5 @@ def rename_files(folder_path):
         os.rename(old_path, new_path)
 
 if __name__ == "__main__":
-    folder = r"C:\Users\McKinley Alex\Desktop\Programming\JS\hammyMemorial\hammy"  # 🔹 change this to your folder
+    folder = r"C:\Users\McKinley Alex\Desktop\Programming\JS\hammyMemorial\vids"  # 🔹 change this to your folder
     rename_files(folder)
